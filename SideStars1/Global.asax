fc07a0c2cb609a408e7f99df84b5fa37f17049ed
MaJimenez <majimenez1114@misena.edu.co>
2020-09-30 17:05:15 -0500

    Se suben los archivos y datos del trabajo SideStar1

---
+++ SideStars1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SideStars1.MvcApplication" Language="C#" %>
---
+++ SideStars1/Models/Series.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SideStars1.Models
+{
+    public class Series
+    {
+        public string Titulo { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Genero { get; set; }
+        public string Idioma { get; set; }
+        public Enum Duracion { get; set; }
+        public string Calidad { get; set; }
+        public string Sinopsis { get; set; }
+    }
+}
